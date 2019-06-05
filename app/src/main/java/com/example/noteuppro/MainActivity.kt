ba91ee46fbac4a068ca07aa7ec773a74ac324039
sahilgrg1 <gargsahil898@gmail.com>
2019-06-05 20:27:09 +0530

    update

--- app/src/main/java/com/example/noteuppro/MainActivity.kt
+++ app/src/main/java/com/example/noteuppro/MainActivity.kt
@@ -5,12 +5,29 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.dialog_layout.view.*
 
 class MainActivity : AppCompatActivity() {
     private val dialogview by lazy {
         layoutInflater.inflate(R.layout.dialog_layout,null,false)
     }
+    private val noteAlert by lazy {
+        AlertDialog.Builder(this)
+            .setTitle("Enter the note here")
+            .setView(dialogview)
+            .setPositiveButton("Save") { dialog, _ ->
+                val note = Notes(
+                    dialogview.etTitle.text.toString(),
+                    dialogview.etDescription.text.toString()
+                )
 
+                dialog.dismiss()
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+            .create()
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,7 +37,9 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
-
+        floatingActionButton.setOnClickListener {
+            noteAlert.show()
+        }
 
     }
 }
