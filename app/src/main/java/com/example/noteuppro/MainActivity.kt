83f4e843303652c3c316609888bff60df0388924
sahilgrg1 <gargsahil898@gmail.com>
2019-06-05 18:24:58 +0530

    code

--- app/src/main/java/com/example/noteuppro/MainActivity.kt
+++ app/src/main/java/com/example/noteuppro/MainActivity.kt
@@ -3,9 +3,14 @@ package com.example.noteuppro
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private val dialogview by lazy {
+        layoutInflater.inflate(R.layout.dialog_layout,null,false)
+    }
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +20,7 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
 
         }
+
 
     }
 }
